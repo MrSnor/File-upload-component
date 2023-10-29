b7019899c7b310340a24befbd7b016b9b67ea52c
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-10-29 10:02:01 +0530

    refactor: add multiple tailwind css files
    to segregate some component specific classes, and make the html appear less messier

--- src/components/FileUploadAndPrev/FileUpload.jsx
+++ src/components/FileUploadAndPrev/FileUpload.jsx
@@ -39,7 +39,7 @@ const FileUpload = (props) => {
       </button>
       <div
         className={cn(
-          "flex cursor-pointer flex-col items-center rounded border-2 border-dashed border-gray-400 bg-gray-100 p-5 py-32 text-gray-500 outline-none transition-colors duration-200 ease-in-out hover:border-gray-700 group-hover:text-gray-700",
+          "dropzone-area",
           {
             "border-blue-400": isFocused,
           },
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./baseTailwind.css";
+// import "./baseTailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
